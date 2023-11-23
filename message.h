450e7b989565828d9744248736c5794108836696
Kibnakamoto <kibnakanoto@protonmail.com>
2023-11-22 21:16:52 -0500

    moved major todos

--- message.h
+++ message.h
@@ -174,15 +174,6 @@ namespace Cryptography
 		return ip;
 	}
 
-	// TODO: MAJOR BUG DETECTED: ADD NO PADDING OPTION TO ALL AES ENCRYPTORS/DECRYPTORS. ALSO CHANGE PT SIZE TO CT SIZE ON PUT FUNCTION CALL - DONE
-	// uses AES256_CBC
-	// TODO: fix keys.cpp, redefine encryption because port key is now different - DONE
-	// TODO: finish off securing the connect function in comm.cpp - DONE
-	// TODO: define send/receive functions in P2P - DONE
-	// NOT DONE:
-	// TODO: define network packet construction and destruction (Packet class, PacketParser class)
-	// TODO: define key exchanging for 2 peer communication 
-	// TODO: define key exchanging for multi peer communication 
 	// key_path: path to keys file
 	// ip: ip address to encrypt
 	// out_len: the new output length. Output is returned
--- torcomm.cpp
+++ torcomm.cpp
@@ -7,6 +7,15 @@
 #include "settings.h"
 #include "keys.h"
 
+	// TODO: MAJOR BUG DETECTED: ADD NO PADDING OPTION TO ALL AES ENCRYPTORS/DECRYPTORS. ALSO CHANGE PT SIZE TO CT SIZE ON PUT FUNCTION CALL - DONE
+	// uses AES256_CBC
+	// TODO: fix keys.cpp, redefine encryption because port key is now different - DONE
+	// TODO: finish off securing the connect function in comm.cpp - DONE
+	// TODO: define send/receive functions in P2P - DONE
+	// NOT DONE:
+	// TODO: define network packet construction and destruction (Packet class, PacketParser class)
+	// TODO: define key exchanging for 2 peer communication 
+	// TODO: define key exchanging for multi peer communication 
 int main()
 {
 	std::string keys_filename = "keys.txt";
