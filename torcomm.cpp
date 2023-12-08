1fd2d8b96a4efde4735d8762590ad3ea0506d8e4
Kibnakamoto <kibnakanoto@protonmail.com>
2023-12-07 20:43:45 -0500

    added todo

--- torcomm.cpp
+++ torcomm.cpp
@@ -9,23 +9,26 @@
 
 ////////// TODOS:
 /* DONE:
- * MAJOR BUG DETECTED: ADD NO PADDING OPTION TO ALL AES ENCRYPTORS/DECRYPTORS. ALSO CHANGE PT SIZE TO CT SIZE ON PUT FUNCTION CALL - DONE
+ * MAJOR BUG DETECTED: ADD NO PADDING OPTION TO ALL AES ENCRYPTORS/DECRYPTORS. ALSO CHANGE PT SIZE TO CT SIZE ON PUT FUNCTION CALL - Oct/Nov
  *AES256_CBC
- * fix keys.cpp, redefine encryption because port key is now different - DONE
- * finish off securing the connect function in comm.cpp - DONE
- * define send/receive functions in P2P - DONE
+ * fix keys.cpp, redefine encryption because port key is now different - Oct/Nov
+ * finish off securing the connect function in comm.cpp - Oct/Nov
+ * define send/receive functions in P2P - Oct/Nov
+ * integrate recv_full, send_full with IVs and HMAC/ECDSA. - Dec 3 2023
  */
 
 /* NOT DONE:
  * TODO: define network packet construction and destruction (Packet class, PacketParser class)
  * TODO: define key exchanging for 2 peer communication 
  * TODO: define key exchanging for multi peer communication 
+ * TODO: add ecdsa support on send/recv and protocol data sizes
  *
  * TODO: while receving and sending fully, make sure that large files are treated properly, don't read the whole data into a byte array but rather read as partitions into an array. This is for really large files where the ram isn't enough,this means remove the Packet/PacketParser classes
  */
 
 /* MAJOR:
- *  TODO: integrate recv_full, send_full with IVs and HMAC/ECDSA. To do so, first encrypt all data at once. This would only apply to data that can fit in the ram available. Otherwise, apply the large data solution defined in the previous todo.
+ *  TODO: hmac/iv with recv_full and send full, this would only apply to data that can fit in the ram available. Otherwise, apply the large data solution defined in the previous todo - Dec 2, 2023
+ *  TODO: MAJOR BUG DETECTED: overflow of uint16_t values because they are assigned uint64_t for networking - Dec 3, 2023 - Possibly debugged, needs further checking - Dec 7, 2023
  */
 
 int main()
