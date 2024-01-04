459ed727ca6925be91035583b63712bf4c45bb98
Kibnakamoto <kibnakanoto@protonmail.com>
2024-01-04 15:22:40 -0500

    debug

--- test/crypto/crypto.cpp
+++ test/crypto/crypto.cpp
@@ -1,6 +1,12 @@
-#include "../message.h"
-#include <cryptopp/gf2n.h>
+#include "../../message.h"
+
+/* Testing Cryptography aspects */
 
 int main()
 {
+	// 1. Test ProtocolData
+	// 2. Test Cipher
+	// 3. Test HMAC
+	// 4. Test Decipher
+	// 5. Test ECDSA (Not Version 1.0)
 }
