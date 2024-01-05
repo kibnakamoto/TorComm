3cf567f4d81eaf9dea6a8b03ba1dd89ff5e39224
Kibnakamoto <kibnakanoto@protonmail.com>
2024-01-05 17:45:05 -0500

    added return ip

--- test/p2p/peer1/peer1.cpp
+++ test/p2p/peer1/peer1.cpp
@@ -1,17 +1,18 @@
 #include <iostream>
 #include <string>
 
-#include "../../message.h"
-#include "../../comm.h"
+#include "../../../message.h"
+#include "../../../comm.h"
 
 const constexpr uint16_t port = 1050;
 
 int main()
 {
-	Blocked blocked = Blocked("../../security/keys", "../../blocked");
+	Blocked blocked = Blocked("../../../security/keys", "../../../blocked");
 	P2P p2p = P2P(port, blocked);
+	std::cout << std::endl << p2p.get_ip() << std::endl << std::flush;
 	p2p.accept();
-	p2p.connect();
+	// p2p.connect();
 	p2p.start_async();
 	return 0;
 }
