1b9714f6e5331a71d430f27939eec557dfbbfbcc
Kibnakamoto <kibnakanoto@protonmail.com>
2024-01-06 22:03:02 -0500

    fixed some functions

--- test/p2p/peer1/peer1.cpp
+++ test/p2p/peer1/peer1.cpp
@@ -8,13 +8,19 @@ const constexpr uint16_t port = 15000;
 
 int main()
 {
-	// std::string their_ip = "2607:fea8:1f1b:3d00:dde3:9b46:d5e7:ed70";
-	// std::string their_ip = "2607:fea8:1f1b:3d00:878:e421:22:a7b";
-	std::string their_ip = "10.0.0.213";
+	Cryptography::Curves curve = Cryptography::SECP256K1;
+	Cryptography::CommunicationProtocol comm_protocol = Cryptography::ECIES_HMAC_AES256_CBC_SHA512;
+	uint8_t protocol_no = (uint8_t)comm_protocol + curve;
+	Cryptography::ProtocolData protocol(protocol_no); // initialize
+	assert(protocol.error == NO_ERROR); // check if there are any errors
+	Cryptography::Key key(protocol);
+
+	std::string their_ip = "2607:fea8:1f1b:3d00:878:e421:22:a7b";
+	// std::string their_ip = "10.0.0.213";
 	Blocked blocked = Blocked("../../../security/keys", "../../../blocked");
 	P2P p2p = P2P(port, blocked);
 	//p2p.accept();
-	p2p.connect(their_ip, port);
+	p2p.connect(their_ip, port, protocol, key);
 	p2p.start_async();
 	return 0;
 }
