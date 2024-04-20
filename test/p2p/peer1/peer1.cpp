5d7699a2704280157b288adeb3c0f4abfa64f3d1
Kibnakamoto <kibnakanoto@protonmail.com>
2024-04-20 14:37:51 -0400

    basic debug

--- test/p2p/peer1/peer1.cpp
+++ test/p2p/peer1/peer1.cpp
@@ -6,6 +6,8 @@
 
 const constexpr uint16_t port = 15000;
 
+// file for testing networking
+
 int main()
 {
 	Cryptography::Curves curve = Cryptography::SECP256K1;
@@ -15,18 +17,22 @@ int main()
 	assert(protocol.error == NO_ERROR); // check if there are any errors
 	Cryptography::Key key(protocol);
 
+	// 2607:fea8:1f1b:3d00:7ed:429f:d15d:24cb
+	// 2607:fea8:1f1b:3d00:1201:4751:332c:3de0 - The One
+
+
 	std::string their_ip = "2607:fea8:1f1b:3d00:878:e421:22:a7b";
 	// std::string their_ip = "10.0.0.213";
 	Blocked blocked = Blocked("../../../security/keys", "../../../blocked");
 	P2P p2p = P2P(port, blocked);
-	p2p.connect(their_ip, port, [&p2p, protocol, &key](boost::asio::ip::tcp::socket &socket) mutable {
-		// p2p.send_two_party_ecdh(socket, protocol, key);
-	});
-
 	p2p.accept([&p2p, protocol, &key](boost::asio::ip::tcp::socket &socket) mutable {
-		ERRORS error;
+		// ERRORS error;
 		// p2p.recv_two_party_ecdh(socket, protocol, key, error);
 	});
+	//p2p.connect(their_ip, port, [&p2p, protocol, &key](boost::asio::ip::tcp::socket &socket) mutable {
+	//	// p2p.send_two_party_ecdh(socket, protocol, key);
+	//});
+
 	p2p.start_async();
 	return 0;
 }
