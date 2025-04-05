c334b54795d2d1967dff8227ce245249f4049096
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-04 22:37:00 -0400

    started gui development

---
+++ ui/CMakeFiles/gui.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui.dir/link.d"
+  "CMakeFiles/gui.dir/gui.cpp.o"
+  "CMakeFiles/gui.dir/gui.cpp.o.d"
+  "gui"
+  "gui.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
