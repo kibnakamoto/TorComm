595ac123a030fb043d89c409eceb54c6710ad715
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-04 22:45:00 -0400

    added todos and semantic changes

--- ui/gui.cpp
+++ ui/gui.cpp
@@ -1,3 +1,23 @@
+ /* Copyright (c) 2023 Taha
+  * this program is free software: you can redistribute it and/or modify
+  * it under the terms of the gnu general public license as published by
+  * the free software foundation, either version 3 of the license, or
+  * (at your option) any later version.
+  * this program is distributed in the hope that it will be useful,
+  * but without any warranty; without even the implied warranty of
+  * merchantability or fitness for a particular purpose.  see the
+  * gnu general public license for more details.
+  * you should have received a copy of the gnu general public license
+  * along with this program.  if not, see <https://www.gnu.org/licenses/>.
+  *
+  * Author: Taha
+  * Date: 2023, Dec 9
+  * Description: This is the main file of this project. This is for people to securely communicate over a public channel using a P2P system (TCP/IPv6). 
+  */
+
+#ifndef GUI_CPP
+#define GUI_CPP
+
 #include <iostream>
 
 #include <QApplication>
@@ -9,8 +29,7 @@ int main(int argc, char *argv[])
     QApplication app(argc, argv);
     Files files;
     GUI gui;
-    //gui.info("here is a message");
-    //gui.warning("here is a warning");
-    gui.error("here is an error");
 	return 0;
 }
+
+#endif /* GUI_CPP */
--- ui/gui.h
+++ ui/gui.h
@@ -1,3 +1,20 @@
+ /* Copyright (c) 2023 Taha
+  * this program is free software: you can redistribute it and/or modify
+  * it under the terms of the gnu general public license as published by
+  * the free software foundation, either version 3 of the license, or
+  * (at your option) any later version.
+  * this program is distributed in the hope that it will be useful,
+  * but without any warranty; without even the implied warranty of
+  * merchantability or fitness for a particular purpose.  see the
+  * gnu general public license for more details.
+  * you should have received a copy of the gnu general public license
+  * along with this program.  if not, see <https://www.gnu.org/licenses/>.
+  *
+  * Author: Taha
+  * Date: 2023, Dec 9
+  * Description: This is the main file of this project. This is for people to securely communicate over a public channel using a P2P system (TCP/IPv6). 
+  */
+
 #ifndef GUI_H
 #define GUI_H
 
@@ -9,6 +26,11 @@
 
 #include <iostream>
 #include <string>
+
+// TODO: Add an interface for a texting tool
+// TODO: Add Documentation while developing
+// TODO: Add color schemes and make sure that all graphics abide the given color schemes
+// TODO: Make a cipher suite selector
 
 class GUI : public QWidget
 {
