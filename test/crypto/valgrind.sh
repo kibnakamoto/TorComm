d7c87f6ac4a3df4286b48b955fdf7a8d49d9b68e
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-14 21:43:01 -0400

    added and passed valgrind tests for p2p

--- test/crypto/valgrind.sh
+++ test/crypto/valgrind.sh
@@ -1,3 +1,4 @@
 # valgrind tests, run after changes to make sure memory management is flawless
+# technically it shows 16 bytes lost, but that error isn't real. It's crypto++ efficiency thing, it can be ignored
 
 valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./crypto
