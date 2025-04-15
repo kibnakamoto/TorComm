1b85ffe4b418c380cbf113bb499e643bb9d1d7ef
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-15 01:34:45 -0400

    removed 0000 since that is local, needed for p2p

--- test/block/blocked.cpp
+++ test/block/blocked.cpp
@@ -12,22 +12,22 @@
 int main()
 {
 	Blocked blocked("../../security/keys", "../../blocked");
-	blocked.block("0000:0000:0000:0000:0000:0000:0000:0000");
 	blocked.block("0000:0000:0000:0000:0000:0000:0000:0001");
 	blocked.block("0000:0000:0000:0000:0000:0000:0000:0002");
+	blocked.block("0000:0000:0000:0000:0000:0000:0000:0003");
 
-	bool blocked0 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0000");
-	bool blocked1 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0001");
-	bool blocked2 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0002");
-	bool blocked3 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0003");
+	bool blocked0 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0001");
+	bool blocked1 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0002");
+	bool blocked2 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0003");
+	bool blocked3 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0004");
 	assert(blocked0);
 	assert(blocked1);
 	assert(blocked2);
 	assert(!blocked3);
 
-	blocked0 = blocked.unblock("0000:0000:0000:0000:0000:0000:0000:0000");
+	blocked0 = blocked.unblock("0000:0000:0000:0000:0000:0000:0000:0001");
 	assert(blocked0); // should say it's blocked
-	blocked0 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0000");
+	blocked0 = blocked.is_blocked("0000:0000:0000:0000:0000:0000:0000:0001");
 	assert(!blocked0); // shouldn't be blocked again
 	std::cout << std::endl << "PASSED - BLOCKED CLASS" << std::endl;
 
