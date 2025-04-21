ac3d9cf8fb76ba4be47f0cd1a25ef67acc16cf84
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-21 17:05:23 -0400

    fixed memory problems with valgrind

--- test/crypto/valgrind.sh
+++ test/crypto/valgrind.sh
@@ -1,4 +1,4 @@
 # valgrind tests, run after changes to make sure memory management is flawless
-# technically it shows 16 bytes lost, but that error isn't real. It's crypto++ efficiency thing, it can be ignored
+# technically it shows 16 bytes lost, but that error isn't real. It's crypto++ efficiency thing, it can be hiden
 
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./crypto
+valgrind --leak-check=full --show-leak-kinds=definite --errors-for-leak-kinds=definite  --track-origins=yes -s ./crypto
