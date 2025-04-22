2310a8ba1a3ca7aaa23450169f62877d66599132
Kibnakamoto <kibnakanoto@protonmail.com>
2025-04-21 21:54:28 -0400

    hid valgrind ecdsa bug

--- test/p2p/valgrind.sh
+++ test/p2p/valgrind.sh
@@ -1,3 +1,3 @@
 # valgrind tests, run after changes to make sure memory management is flawless
 
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes -s ./p2p
+valgrind --leak-check=full --show-leak-kinds=definite --errors-for-leak-kinds=definite --track-origins=yes -s ./p2p
